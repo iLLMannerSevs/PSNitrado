701740c740ece991c0f5601e7d309f457228dcf2
Falk Heiland <falkheiland@gmail.com>
2019-10-31 13:01:48 +0100

    .

--- PSNitrado/Private/Get-NitradoPropertyCast.ps1
+++ PSNitrado/Private/Get-NitradoPropertyCast.ps1
@@ -17,95 +17,24 @@ function Get-NitradoPropertyCast
 
   begin
   {
-    #$APIObjectColl
-    #$PropertyColl
-
   }
   process
   {
-    $Result = foreach ($APIObject in $APIObjectColl)
+    $TypeColl = $PropertyColl.GetEnumerator().Name
+    $Result = foreach ($Type in $TypeColl)
     {
-      $CastedPropertyColl = @{ }
-      <#
-      foreach ($ObjectProperty in $PropertyColl.Object)
+      #$Type
+      foreach ($Property in $PropertyColl.$($Type))
       {
-        $Name = ((Get-Culture).TextInfo.ToTitleCase($ObjectProperty).Split('_') -join '').Replace(' ', '')
-        if ($APIObject.($ObjectProperty))
-        {
-          $ChildColl = $APIObject.($ObjectProperty) | Get-Member -Type NoteProperty | Foreach-Object {
-            @{
-              $_.Name = ($APIObject.($ObjectProperty))."$($_.Name)"
-            }
-          }
-          #$CastedPropertyColl.Add(('{0}{1}' -f $Name, $_.Name), $Value)
-          $Value = Get-NitradoPropertyCast -APIObjectColl $ChildColl -PropertyColl $PropertyColl
-          $CastedPropertyColl.Add(('{0}{1}' -f $Name, $_.Name), $Value)
+        $Name = ((Get-Culture).TextInfo.ToTitleCase($Property).Split('_').Split('.') -join '').Replace(' ', '')
+        $Value = (Invoke-Expression "`$APIObjectColl.$Property")
+        $Properties = @{
+          $Name = $Value
         }
-        #$CastedPropertyColl.Add($Name, $Value)
+        $Properties
       }
-      #>
-      foreach ($StringProperty in $PropertyColl.String)
-      {
-        if ($StringProperty -eq 'saddress')
-        {
-          $StringProperty = 'ipaddress'
-        }
-        $Name = ((Get-Culture).TextInfo.ToTitleCase($StringProperty).Split('_') -join '').Replace(' ', '')
-        if ($APIObject.($StringProperty))
-        {
-          $Value = [String]$APIObject.($StringProperty)
-          $CastedPropertyColl.Add($Name, $Value)
-        }
-      }
-      foreach ($IntProperty in $PropertyColl.Int)
-      {
-        $Name = ((Get-Culture).TextInfo.ToTitleCase($IntProperty).Split('_') -join '').Replace(' ', '')
-        if ($APIObject.($IntProperty))
-        {
-          $Value = [Int]$APIObject.($IntProperty)
-          $CastedPropertyColl.Add($Name, $Value)
-        }
-      }
-      foreach ($BoolProperty in $PropertyColl.Bool)
-      {
-        $Name = ((Get-Culture).TextInfo.ToTitleCase($BoolProperty).Split('_') -join '').Replace(' ', '')
-        if ($APIObject.($BoolProperty))
-        {
-          $Value = [System.Convert]::ToBoolean($APIObject.($BoolProperty))
-          $CastedPropertyColl.Add($Name, $Value)
-        }
-      }
-      foreach ($DatetimeProperty in $PropertyColl.Datetime)
-      {
-        $Name = ((Get-Culture).TextInfo.ToTitleCase($DatetimeProperty).Split('_') -join '').Replace(' ', '')
-        if ($APIObject.($DatetimeProperty))
-        {
-          $Value = [System.Convert]::ToDateTime($APIObject.($DatetimeProperty))
-          $CastedPropertyColl.Add($Name, $Value)
-        }
-      }
-      foreach ($ArrayProperty in $PropertyColl.Array)
-      {
-        $Name = ((Get-Culture).TextInfo.ToTitleCase($ArrayProperty).Split('_') -join '').Replace(' ', '')
-        if ($APIObject.($ArrayProperty))
-        {
-          $Value = [array]$APIObject.($ArrayProperty)
-          $CastedPropertyColl.Add($Name, $Value)
-        }
-      }
-      foreach ($ObjectProperty in $PropertyColl.Object)
-      {
-        $Name = ((Get-Culture).TextInfo.ToTitleCase($ObjectProperty).Split('_') -join '').Replace(' ', '')
-        if ($APIObject.($ObjectProperty))
-        {
-          $Value = [pscustomobject]$APIObject.($ObjectProperty)
-          $CastedPropertyColl.Add($Name, $Value)
-        }
-      }
-      New-Object psobject -Property $CastedPropertyColl
     }
-    $Result #| Get-Member
-    #>
+    $Result
   }
   end
   {
--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -5,12 +5,13 @@ $TokenName = '.nitradotoken'
 [int]$GameserverId = 4089632
 
 $Token = Get-NitradoToken -Path ('{0}/{1}' -f $TokenPath, $TokenName)
-Get-NitradoGameserverBackup -Token $Token -Id $GameserverId
+$Result = Get-NitradoUser -Token $Token
+$Result
 
 <#
+Get-NitradoGameserverBackup -Token $Token -Id $GameserverId
 Get-NitradoGameserver -Token $Token -Id $GameserverId
 Get-NitradoService -Token $Token
 Get-NitradoPing -Token $Token
-Get-NitradoUser -Token $Token
 Get-NitradoMaintenance -Token $Token
 #>
