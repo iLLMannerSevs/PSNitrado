a586029f330803b36d95cd61c661794dd10de3a5
Falk <falkheiland@googlemail.com>
2019-11-04 16:29:53 +0100

    Update Test.ps1

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -11,17 +11,19 @@ $File = '/games/ni1429468_1/noftp/dayzps/config/DayZServer_PS4_x64.ADM'
 $DestDir = [IO.Path]::GetTempPath()
 
 $Token = Get-NitradoToken -Path ('{0}/{1}' -f $TokenPath, $TokenName)
+$Result = Get-NitradoService -Token $Token -Id 4558109
+$Result
+<#
+$Result = Get-NitradoService -Token $Token
+$Result = Get-NitradoUser -Token $Token
 $Result = (Get-NitradoGameserverFile -Token $Token -Id $GameserverId -Dir $SourceDir -Search $Search).foreach{
   Export-NitradoGameserverFile -Token $Token -Id $GameserverId -Path $DestDir -File $_.path
 }
-$Result
-<#
 $Result = Get-NitradoGameserverFile -Token $Token -Id $GameserverId -Dir $SourceDir -Search $Search
 $Result = Export-NitradoGameserverFile -Token $Token -Id $GameserverId -Path $DestDir -File $File
 $Result = Get-NitradoGameserver -Token $Token -Id $GameserverId
-$Result = Get-NitradoUser -Token $Token
 $Result = Get-NitradoGameserverBackup -Token $Token -Id $GameserverId
-$Result = Get-NitradoService -Token $Token
 $Result = Get-NitradoPing -Token $Token
 $Result = Get-NitradoMaintenance -Token $Token
+$Result = Get-NitradoToken -Path ('{0}/{1}' -f $TokenPath, $TokenName)
 #>
