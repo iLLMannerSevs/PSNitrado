b72c644d3a9cfbcd8d4d6d92723577eee516b35e
falkheiland <falkheiland@users.noreply.github.com>
2019-11-10 11:26:18 +0100

    Update Get-NitradoGameserverBackup.ps1

--- PSNitrado/Public/Get-NitradoGameserverBackup.ps1
+++ PSNitrado/Public/Get-NitradoGameserverBackup.ps1
@@ -1,18 +1,5 @@
 function Get-NitradoGameserverBackup
 {
-  <#
-    .SYNOPSIS
-
-    .DESCRIPTION
-
-    .PARAMETER Param1
-
-    .PARAMETER Param2
-
-    .EXAMPLE
-    Get-NitradoMaintenance -Token $Token
-  #>
-
   [CmdletBinding()]
   Param
   (
@@ -20,7 +7,7 @@ function Get-NitradoGameserverBackup
     [string]
     $Token,
 
-    [Parameter(Mandatory)]
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [int]
     $Id
   )
